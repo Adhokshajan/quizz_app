de8205bae235bd688a08e4aef1583eb331fe43be
Adhokshajan <r.adhokshajan@gmail.com>
2024-09-23 23:06:34 +0530

    update

--- main.dart
+++ main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:quiz_app/home_page.dart';
 import 'package:quiz_app/quiz.dart';
 
 void main() {
